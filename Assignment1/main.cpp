5d9aee85a72fb7d1848026b143d8c80caee98720
An Vu <anvu94@gmail.com>
2016-02-09 04:10:29 -0500

    starting the assignment

--- Assignment1/main.cpp
+++ Assignment1/main.cpp
@@ -1,4 +1,4 @@
-5763#ifdef _WINDOWS
+#ifdef _WINDOWS
 	#include <GL/glew.h>
 #endif
 #include <SDL.h>
@@ -13,10 +13,42 @@
 
 SDL_Window* displayWindow;
 
+//create 2d scene using textured polygons
+//one element must be animated
+//3 different textures
+
+GLuint LoadTexture(const char *img){
+	SDL_Surface *surface = IMG_Load(img);
+	GLuint textureID;
+	glGenTextures(1, &textureID);
+	glBindTexture(GL_TEXTURE_2D, textureID);
+	glTexImage2D(GL_TEXTURE_2D, 0, 4, surface->w, surface->h, 0, GL_RGBA, GL_UNSIGNED_BYTE, surface->pixels);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	SDL_FreeSurface(surface);
+	return textureID;
+}
+
+void draw(GLuint texture, float x, float y, float scale, float rotation = 0){
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glTranslatef(x, y, 0.0);
+	glRotatef(rotation, 0.0, 0.0, 0.0);
+	GLfloat coord[] = { 0.0f, 0.1f, 0.0f, -0.1f, 0.0f, 0.1f, 0.0f, 0.1f);
+	glVertexPointer(2, GL_FLOAT, 0, coord);
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glDrawArrays(GL_QUADS, 0, 4);
+	glDisable(GL_TEXTURE_2D);
+}
+
 int main(int argc, char *argv[])
 {
 	SDL_Init(SDL_INIT_VIDEO);
-	displayWindow = SDL_CreateWindow("My Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 360, SDL_WINDOW_OPENGL);
+	displayWindow = SDL_CreateWindow("Assignment 1", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 360, SDL_WINDOW_OPENGL);
 	SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
 	SDL_GL_MakeCurrent(displayWindow, context);
 	#ifdef _WINDOWS
@@ -25,13 +57,34 @@ int main(int argc, char *argv[])
 
 	SDL_Event event;
 	bool done = false;
+	
+	//size and pixel offset
+	glViewport(0, 0, 640, 360);
+	//projection matrix
+	glMatrixMode(GL_PROJECTION);
+	//load identity matrix
+	glLoadIdentity();
+	glMatrixMode(GL_MODELVIEW);
+	
+	float lastFrameTicks = 0.0f;
+
 	while (!done) {
 		while (SDL_PollEvent(&event)) {
 			if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
 				done = true;
 			}
 		}
+		//tick tick tick
+		float ticks = (float)SDL_GetTicks / 1000.0f;
+		float elapsed = ticks - lastFrameTicks;
+		lastFrameTicks = ticks;
+
 		glClear(GL_COLOR_BUFFER_BIT);
+
+
+
+		glDisableClientState(GL_VERTEX_ARRAY);
+
 		SDL_GL_SwapWindow(displayWindow);
 	}
 
