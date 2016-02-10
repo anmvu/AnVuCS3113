f8533702f2020603006dc22be9a27e6e810d378a
An Vu <anvu94@gmail.com>
2016-02-09 20:54:25 -0500

    got the road working

--- Assignment1/main.cpp
+++ Assignment1/main.cpp
@@ -4,6 +4,7 @@
 #include <SDL.h>
 #include <SDL_opengl.h>
 #include <SDL_image.h>
+#include "Matrix.h"
 #include "ShaderProgram.h"
 
 #define PI 3.14159265
@@ -20,30 +21,49 @@ SDL_Window* displayWindow;
 //one element must be animated
 //3 different textures
 
-GLuint LoadTexture(const char *img){
+GLuint LoadTexture(const char *img, int type){
 	SDL_Surface *surface = IMG_Load(img);
 	GLuint textureID;
 	glGenTextures(1, &textureID);
 	glBindTexture(GL_TEXTURE_2D, textureID);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, surface->w, surface->h, 0, GL_RGBA, GL_UNSIGNED_BYTE, surface->pixels);
+	if (type == GL_RGBA){
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, surface->w, surface->h, 0, GL_RGBA, GL_UNSIGNED_BYTE, surface->pixels);
+	}
+	else if (type == GL_RGB){
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, surface->w, surface->h, 0, GL_RGB, GL_UNSIGNED_BYTE, surface->pixels);
+	}
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	SDL_FreeSurface(surface);
 	return textureID;
 }
 
-void draw(ShaderProgram program,GLuint texture){
+void draw(ShaderProgram program,GLuint texture, float x, float y, float rotation = 0){
+	//Blend
 	glEnable(GL_TEXTURE_2D);
 	glBindTexture(GL_TEXTURE_2D, texture);
-	float vertices[] = { -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5 };
+	
+	//program
+	glUseProgram(program.programID);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glTranslatef(x, y, 0.0);
+	glRotatef(rotation, 0.0, 0.0, 0.0);
+	float vertices[] = { -1, -1, 1, -1, 1, 1 ,- 1, -1, 1, 1, -1, 1 };
 	glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
 	glEnableVertexAttribArray(program.positionAttribute);
-	float coords[] = { 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0 };
+	//screen is -3.55f, 3.55f, -2.0f, 2.0f
+	float coords[] = { 0.0, 0.0, 1.0, 0.0, 1.0, 1.0,0.0, 0.0, 1.0, 1.0, 0.0, 1.0 };
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glDisable(GL_TEXTURE_2D);
 	glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, coords);
 	glEnableVertexAttribArray(program.texCoordAttribute);
 	glDrawArrays(GL_TRIANGLES, 0, 6);
 	glDisableVertexAttribArray(program.positionAttribute);
 	glDisableVertexAttribArray(program.texCoordAttribute);
+
 }
 
 int main(int argc, char *argv[])
@@ -62,9 +82,9 @@ int main(int argc, char *argv[])
 	//size and pixel offset
 	glViewport(0, 0, 640, 360);
 	ShaderProgram program(RESOURCE_FOLDER"vertex_textured.glsl", RESOURCE_FOLDER"fragment_textured.glsl");
-	GLuint street = LoadTexture("street.png");
-	GLuint car = LoadTexture("car.png");
-	GLuint moon = LoadTexture("moon.png");
+	GLuint street = LoadTexture("street.png", GL_RGBA);
+	GLuint car = LoadTexture("car.png",GL_RGB);
+	GLuint moon = LoadTexture("moon.png",GL_RGBA);
 
 	Matrix projectionMatrix;
 	Matrix modelMatrix;
@@ -92,17 +112,17 @@ int main(int argc, char *argv[])
 		float elapsed = ticks - lastFrameTicks;
 		lastFrameTicks = ticks;
 
+		glClear(GL_COLOR_BUFFER_BIT);
+
 		//drawing
 		program.setModelMatrix(modelMatrix);
 		program.setProjectionMatrix(projectionMatrix);
 		program.setViewMatrix(viewMatrix);
-		draw(program,street);
+		draw(program,street,0,0);
 		//draw(program, car);
-		draw(program,moon);
-		glClear(GL_COLOR_BUFFER_BIT);
+		//draw(program,moon);
 
-
-
+	
 		glDisableClientState(GL_VERTEX_ARRAY);
 
 		SDL_GL_SwapWindow(displayWindow);
